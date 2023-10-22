f52b19e41e37f2f11f48ed6ad206f0104cb44ecf
Elisa Nika <elisanika03@Elisas-MacBook-Pro.local>
2023-10-22 19:40:51 -0400

    Made changes to the front end

--- mentormark/src/App.js
+++ mentormark/src/App.js
@@ -1,33 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Mentor Mark
-        </p>
-        <div>
-                <button onClick={() => console.log("Refer to Sign up page.")}>
-                    Sign Up
-                </button>
-                <button onClick={() => console.log("Refer to Log In page.")}>
-                    Log In
-                </button>
-            </div>{" "}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="top-bar">
+          <div className="buttons">
+            <button
+              className="sign-up-button"
+              onClick={() => console.log("Refer to Sign up page.")}
+            >
+              Sign Up
+            </button>
+            <div className="log-in-button-frame">
+              <button
+                className="log-in-button"
+                onClick={() => console.log("Refer to Log In page.")}
+              >
+                Log In
+              </button>
+            </div>
+          </div>
+          <p className="app-name">Mentor Mark</p>
+        </div>
       </header>
     </div>
   );
