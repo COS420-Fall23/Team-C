10394f149347890c2b2cc9cfd03075e63327c2f7
Elisa Nika <elisanika03@Elisas-MacBook-Pro.local>
2023-11-06 17:32:48 -0500

    everything is added for the front-page

--- mentormark/src/App.js
+++ mentormark/src/App.js
@@ -1,27 +1,52 @@
 import "./App.css";
+import MentorMarkLogo from "./logo/MentorMarkLogoFinals-12.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="top-bar">
-          <div className="buttons">
+        <div className="left-section">
+          <div className="branding">
+            <img src={MentorMarkLogo} alt="MentorMark Logo" className="logo" />
+            <div className="big-text">entorMark</div>
+            <div className="small-text">Imposter no more!</div>
+          </div>
+          <div className="signup-login-section">
+            <div className="input-container">
+              <label className="label" htmlFor="email">
+                Email:
+              </label>
+              <input
+                className="input"
+                type="email"
+                id="email"
+                placeholder="Enter your email"
+              />
+            </div>
+            <div className="input-container">
+              <label className="label" htmlFor="password">
+                Password:
+              </label>
+              <input
+                className="input"
+                type="password"
+                id="password"
+                placeholder="Enter your password"
+              />
+            </div>
+            <button
+              className="log-in-button"
+              onClick={() => console.log("Refer to Log In page.")}
+            >
+              Log In
+            </button>
             <button
               className="sign-up-button"
               onClick={() => console.log("Refer to Sign up page.")}
             >
               Sign Up
             </button>
-            <div className="log-in-button-frame">
-              <button
-                className="log-in-button"
-                onClick={() => console.log("Refer to Log In page.")}
-              >
-                Log In
-              </button>
-            </div>
           </div>
-          <p className="app-name">Mentor Mark</p>
         </div>
       </header>
     </div>
