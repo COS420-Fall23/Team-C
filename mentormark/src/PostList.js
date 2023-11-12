8a48e0877c3e0e52379283cdfc4fce14c5aeb803
Jonathan Malcolm <143540979+Unicarian@users.noreply.github.com>
2023-11-12 13:51:18 -0500

    Added Post List View

--- mentormark/src/PostList.js
+++ mentormark/src/PostList.js
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom"
-
-//Calls database manager and gets a list of posts
-function getPostList() {
-
-}
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { collection } from "firebase/firestore";
+import { db } from "./firebase-config";
 
 //Creates a list of Post Titles with links to a post component.
 //When clicked, replaces/redirects post list to post
-export default function PostList() {
+export default function PostListView() {
+    const [value, loading, error] = useCollection(collection(db, 'posts'));
+
     return <div>
-        <Link />
-        <ul>
-            
-        </ul>
-    </div>
+    {(value)? <div>
+      {value.docs.map((obj) => (
+        <div>
+          <div>Title: {obj.data().Title}</div>
+        </div>
+      ))}
+    </div> : <div>Loading Posts</div>}
+  </div>
 }
