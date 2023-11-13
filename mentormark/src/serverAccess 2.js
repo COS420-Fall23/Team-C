615f56804aaa99214c0209c1b115a4265b966d35
elisanika0333 <117917242+elisanika0333@users.noreply.github.com>
2023-11-13 16:42:52 -0500

    SignUpForm

---
+++ mentormark/src/serverAccess 2.js
@@ -0,0 +1,24 @@
+// React component
+import React from "react";
+
+class serverAccess extends React.Component {
+  createFile = (data) => {
+    // Define the data you want to send
+    const sData = {
+      fileName: "accounts.txt",
+      fileContent: data,
+    };
+    fetch("/createFile", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json", // Set the content type to JSON
+      },
+      body: JSON.stringify(sData), // Convert data to JSON string
+    })
+      .then((response) => response.text())
+      .then((message) => console.log(message))
+      .catch((error) => console.error("Error:", error));
+  };
+}
+
+export default serverAccess;
