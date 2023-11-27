51420fba4e4b6a4fd89662763f56886221ffdc6f
Jonathan Malcolm <143540979+Unicarian@users.noreply.github.com>
2023-11-27 16:31:41 -0500

    Cleaned up post tests

--- mentormark/src/Test/Post.test.js
+++ mentormark/src/Test/Post.test.js
@@ -1,22 +1,3 @@
-// import * as React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import Post from '../Post';
-// import { MemoryRouter } from 'react-router-dom';
-
-// const testPost = {title: "Test 1", content: "This is the first test post"}
-
-// describe("Post", () => {
-//     test("Post renders", async () => {
-//         render(<MemoryRouter><Post toChild={testPost} /></MemoryRouter>);
-//         expect(screen.getByRole('link')).toBeInTheDocument();
-//         expect(screen.getByText(/Test 1/)).toBeInTheDocument();
-//     });
-//     test("Post fails ot render with bad/no props", async () => {
-//         render(<MemoryRouter><Post toChild={null} /></MemoryRouter>);
-//         expect(screen.getByText(/Error loading post/)).toBeInTheDocument();
-//     });
-// });
-
 import { render, fireEvent, cleanup, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Post from '../Post';
